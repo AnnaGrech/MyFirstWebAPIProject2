28618ba72ac783c5bb8d6ba4f62df32bb0f52ce4
agr22@mail.ru <agr22@mail.ru>
2024-08-14 10:07:17 +0300

    Add model 'Task'

---
+++ MyFirstWebAPIProject/Models/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstWebAPIProject.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+        public string Title { get; set; }
+        public string Diskription { get; set; }
+    }
+}
